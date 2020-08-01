8d0192812507aa45c911f0b34b57da920833e6de
Gerson Goatache <gersongoatache@gmail.com>
2020-08-01 11:52:44 -0400

    mock data in case

---
+++ src/data.js
@@ -0,0 +1,9 @@
+export const mock = [
+        {
+            name: "Song name",
+            duration: 220,
+            artist: "Gerson Goatache",
+            id: 1,
+            image: 'url'
+        }
+];
