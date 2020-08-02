8507a5185975de208fa9ff3e024a5ecf1cf6b7e4
Gerson Goatache <gersongoatache@gmail.com>
2020-08-02 17:42:10 -0400

    musicr url

--- src/data.js
+++ src/data.js
@@ -4,13 +4,15 @@ export const mock = [
             duration: 220,
             artist: "Gerson Goatache",
             id: 1,
-            image: 'url'
+            image: 'url',
+            music: 'https://d2tml28x3t0b85.cloudfront.net/tracks/stream_files/000/696/722/original/Bomba%20Est%C3%A9reo%20-%20To%20My%20Love%20%28Moombahton%20Bootleg%29.mp3?1514668785',
         },
         {
             name: "Second track",
             duration: 150,
             artist: "Carla Ramirez",
             id: 2,
-            image: 'url'
+            image: 'url',
+            music: 'https://d2tml28x3t0b85.cloudfront.net/tracks/stream_files/000/696/722/original/Bomba%20Est%C3%A9reo%20-%20To%20My%20Love%20%28Moombahton%20Bootleg%29.mp3?1514668785',
         }
 ];
