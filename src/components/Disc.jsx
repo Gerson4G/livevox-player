96094bec2c6e6172d4809a6c6d7fcc7b208dfdb8
Gerson Goatache <gersongoatache@gmail.com>
2020-08-02 20:40:11 -0400

    image disc

--- src/components/Disc.jsx
+++ src/components/Disc.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCompactDisc } from '@fortawesome/free-solid-svg-icons'
+import palette from '../constants';
+import { useEffect } from 'react';
 
 const radius = '3em';
 
 const StyledDisc = styled.div`
     border-radius: 3em;
-    background-color: red;
+    background-color: ${palette.pasive};
     height: ${radius};
     width: ${radius};
     align-self: flex-start;
@@ -15,6 +17,10 @@ const StyledDisc = styled.div`
     align-items: center;
     justify-content: center;
     align-self: center;
+
+    img {
+        border-radius: 30px;
+    }
 
     @keyframes rotating {
         from {
@@ -25,17 +31,26 @@ const StyledDisc = styled.div`
         }
     }
 
-    .disc-image {
+    .disc-image, img {
         height: 90%;
         width: auto;
-        ${({isPlaying}) =>isPlaying && 'animation: rotating 2s linear infinite;' }
+        ${({isPlaying}) =>isPlaying && 'animation: rotating 4s linear infinite;' }
     }
 `;
 
-const Disc = ({isPlaying}) => {
+const Disc = ({isPlaying, image}) => {
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        setError(false);
+    }, [image])
+
     return(
         <StyledDisc isPlaying={isPlaying}>
-            <FontAwesomeIcon className="disc-image" icon={faCompactDisc} />
+            {   !error ?
+                <img src={image} onError={() => setError(true)}></img> :
+                <FontAwesomeIcon className="disc-image" icon={faCompactDisc} />
+            }
         </StyledDisc>
     )
 };
