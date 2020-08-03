c7c6c6f04ddf3147a483808a1007bc9864068b81
Gerson Goatache <gersongoatache@gmail.com>
2020-08-02 20:39:34 -0400

    another track

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const mock = [
             duration: 220,
             artist: "Gerson Goatache",
             id: 1,
-            image: 'url',
+            image: 'https://i.pinimg.com/236x/f6/4a/61/f64a618f9e86a23fe73a4587196009d5--michael-jackson-bad-michael-okeefe.jpg',
             music: 'https://d2tml28x3t0b85.cloudfront.net/tracks/stream_files/000/696/722/original/Bomba%20Est%C3%A9reo%20-%20To%20My%20Love%20%28Moombahton%20Bootleg%29.mp3?1514668785',
         },
         {
@@ -13,6 +13,14 @@ export const mock = [
             artist: "Carla Ramirez",
             id: 2,
             image: 'url',
+            music: 'https://d2tml28x3t0b85.cloudfront.net/tracks/stream_files/000/696/722/original/Bomba%20Est%C3%A9reo%20-%20To%20My%20Love%20%28Moombahton%20Bootleg%29.mp3?1514668785',
+        },
+        {
+            name: "third track",
+            duration: 333,
+            artist: "Agustin Bossi",
+            id: 3,
+            image: 'https://i.pinimg.com/236x/6a/e6/4a/6ae64aab371af35410e719ac7d704195--madonna-cd-madonna-albums.jpg',
             music: 'https://d2tml28x3t0b85.cloudfront.net/tracks/stream_files/000/696/722/original/Bomba%20Est%C3%A9reo%20-%20To%20My%20Love%20%28Moombahton%20Bootleg%29.mp3?1514668785',
         }
 ];
