ee3f03c5f4c8b13f25a4ad5102ac691370768819
Gerson Goatache <gersongoatache@gmail.com>
2020-08-02 20:06:52 -0400

    popover position

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -5,6 +5,7 @@ import ProgressBar from './ProgressBar';
 const DescriptionContainer = styled.div`
     display: flex;
     align-items: center;
+
     span:first-child {
         cursor: pointer;
     }
